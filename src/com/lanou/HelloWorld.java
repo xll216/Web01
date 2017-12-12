8aa969602469cf038f3099ba163085e09f5c03ab
xll216 <xll_216@163.com>
2017-12-12 13:29:24 +0800

    初始化代码

---
+++ src/com/lanou/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.lanou;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+
+	private String message;
+
+	@Override
+	public void init() throws ServletException {
+		// 执行必需的初始化
+		message = "Hello World";
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 设置响应内容类型
+		response.setContentType("text/html");
+
+		// 实际的逻辑是在这里
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+	}
+
+	@Override
+	public void destroy() {
+		// 什么也不做
+	}
+
+}
